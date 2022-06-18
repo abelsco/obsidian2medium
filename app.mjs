695ac0128e5f15f1adec770c9a7b579d3983463e
Abel Menezes <abelsmenezes@gmail.com>
2022-06-18 17:55:42 -0300

    adicionando app.mjs

---
+++ app.mjs
@@ -0,0 +1,72 @@
+import express from 'express'
+import fetch from "node-fetch"
+import fs from 'fs/promises'
+import bodyParser from 'body-parser'
+import 'dotenv/config'
+const app = express()
+const port = 3000
+app.use(bodyParser.json({ type: 'application/*+json' }))
+var jsonParser = bodyParser.json()
+
+async function getAuth() {
+  const api_fetch = await fetch(`${process.env.API}/me`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const response = await api_fetch.json();
+
+  return response.data
+}
+
+async function openFile(name) {
+  try {
+    const data = await fs.readFile(`${process.env.OBSIDIAN_FOLDER}/${name}.md`, { encoding: 'utf8' })
+    return data
+  } catch (err) {
+    throw err
+  }
+}
+
+async function bodyParserContent(fileName) {
+  const file = await openFile(fileName)
+  const body = {
+    "title": "Liverpool FC",
+    "contentFormat": "markdown",
+    "content": file,
+    "tags": ["football", "sport", "Liverpool"],
+    "publishStatus": "draft"
+  }
+
+  console.log(body)
+
+  return JSON.stringify(body)
+}
+
+app.get('/', async (req, res) => {
+  const app = await getAuth()
+  res.status(200).json({ data: app })
+})
+
+app.post('/post', jsonParser, async (req, res) => {
+  const app = await getAuth()
+  console.log(req.body)
+  const body = await bodyParserContent(req.body.fileName)
+  const api_fetch = await fetch(`${process.env.API}/users/${app.id}/posts`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.TOKEN}`,
+      'Accept-Charset': 'utf-8'
+    },
+    body: body
+  })
+  const data = await api_fetch.json()
+  res.status(200).json({ api: data })
+})
+
+app.listen(port, () => {
+  console.log(`[***] servidor rodando em ${port}`)
+})
