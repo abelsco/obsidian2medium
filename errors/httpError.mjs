b7f0a764c5d17d6d2af1c77448b373fa8c7e29f5
Abel Menezes <abelsmenezes@gmail.com>
2022-06-20 01:44:48 -0300

    adicionando módulo de tratamento de erros para as requisições http

---
+++ errors/httpError.mjs
@@ -0,0 +1,24 @@
+export class HTTPResponseError extends Error {
+  constructor(response, ...args) {
+    super(`HTTP Error Response: ${response.status} ${response.statusText}`, ...args)
+    this.response = response
+    this.messageResponse = {
+      api: {
+        url: response.url,
+        type: response.statusText
+      },
+      status: response.status
+    }
+
+  }
+}
+
+export function checkStatus(response) {
+  if (response.ok) {
+    // response.status >= 200 && response.status < 300
+    return response;
+  } else {
+    throw new HTTPResponseError(response);
+  }
+}
+
